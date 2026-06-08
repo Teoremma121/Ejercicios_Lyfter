28709b83560eb417be970b9b00432b75cbf57643
Emmanuel Piedra <epiedra121@gmail.com>
2026-06-08 16:10:53 -0600

    Ejercicios Finalizados

--- 04_Python_Intermedio/06_Ejercicios_Análisis_de_Algoritmos/Análisis_Algoritmos_01.py
+++ 04_Python_Intermedio/06_Ejercicios_Análisis_de_Algoritmos/Análisis_Algoritmos_01.py
@@ -1,9 +1,25 @@
-# Ejercicios de Análisis de Algoritmos -- Python Intermedio
-# Emmanuel Piedra Esquivel
-# Programa 01:
+#  Ejercicios de Análisis de Algoritmos -- Python Intermedio
+#  Emmanuel Piedra Esquivel
+#  Ejercicio 01:
 
-# Analice el algoritmo de bubble_sort usando la Big O Notation:
+#  Analice el algoritmo de bubble_sort usando la Big O Notation:
 
+def bubble_sort(list_for_sort):
+    print(f"Orden inicial: {list_for_sort}") #O(1)
+    for round in range(len(list_for_sort)): #O(n)
+        iterated_element = list_for_sort[0] #O(n)
+        for iteration in range(len(list_for_sort)-round-1): #O(n^2) 
+            if iterated_element > list_for_sort[iteration+1]: #O(n^2)
+                list_for_sort[iteration] = list_for_sort[iteration+1] #O(n^2)
+                list_for_sort[iteration+1] = iterated_element #O(n^2)
+            else:
+                iterated_element = list_for_sort[iteration+1] #O(n^2)
+            print(f"Recorrido: {round+1}, Iteración: {iteration+1}, {list_for_sort}") #O(n^2)
+    print(f"Orden final: {list_for_sort}") #O(1)
+    return list_for_sort #O(1)
+
+
+# * Complejidad del algoritmo: O(n^2)
 
 
 
