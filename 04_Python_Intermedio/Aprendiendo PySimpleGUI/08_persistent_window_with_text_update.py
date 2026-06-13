834b117091968dd7053f724607ddda43a5f97558
Emmanuel Piedra <epiedra121@gmail.com>
2026-06-13 09:08:13 -0600

    Más ejemplos PySimpleGUI

---
+++ 04_Python_Intermedio/Aprendiendo PySimpleGUI/08_persistent_window_with_text_update.py
@@ -0,0 +1,27 @@
+import PySimpleGUI as sg
+
+sg.theme('Dark Blue 3')
+
+layout = [ [sg.Txt('Enter values to calculate')],
+            [sg.In(size=(8,1), key='-NUMERATOR-')],
+            [sg.Txt('_'  * 10)],
+            [sg.In(size=(8,1), key='-DENOMINATAOR-')],
+            [sg.Txt(size=(8,1), key='-OUTPUT-')  ],
+            [sg.Button('Calculate', bind_return_key=True)]]
+
+window = sg.Window('Math', layout)
+
+while True:
+    event, values = window.read()
+
+    if event != sg.WIN_CLOSED:
+        try:
+            numerator = float(values['-NUMERATOR-'])
+            denominator = float(values['-DENOMINATAOR-'])
+            calc = numerator/denominator
+        except:
+            calc = 'Invalid'
+
+        window['-OUTPUT-'].update(calc)
+    else:
+        break
